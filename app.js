6c99a630504737ea1b7be2e35d34216df76e0002
Bird Tinapat <bbird0662@gmail.com>
2025-01-12 20:18:09 +0700

    [vercel]

--- app.js
+++ app.js
@@ -8,11 +8,19 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send('Server is up and running!');
+});
+
 // Use the webhook routes
 app.use('/webhook', webhookRoutes);
 
+if (!process.env.VERCEL) {
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+}
+
+module.exports = app; // สำคัญสำหรับ Vercel Dev
 
